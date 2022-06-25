cd83b155e2bdc8a7602e7388b4c53db520b04c1a
Graham <graham.lim88@gmail.com>
2022-06-25 17:51:09 +0800

    config files updated

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    username: '<YOUR_USER_NAME>',
+    username: 'grahamlim',
     password: null,
-    database: 'cra_ecom_development',
+    database: 'project_mgmt_app_development',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
