7fa82a04504287dd00b5540c116b00341744451a
Graham <graham.lim88@gmail.com>
2022-06-28 01:43:06 +0800

    added seed data from industries and skills

---
+++ seeders/20220627172902-industries-data.js
@@ -0,0 +1,36 @@
+module.exports = {
+  up: async (queryInterface) => {
+    const startingIndustries = [
+      {
+        name: 'Airline',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Finance',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Market Research',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Human Resources',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Technology',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ];
+    queryInterface.bulkInsert('industries', startingIndustries);
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.bulkDelete('users', null);
+  },
+};
---
+++ seeders/20220627173217-skills-data.js
@@ -0,0 +1,61 @@
+module.exports = {
+  up: async (queryInterface) => {
+    const startingSkills = [
+      {
+        name: 'JavaScript',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'React.js',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Node.js',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Express.js',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Sequelize',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'PostgreSQL',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Python',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Data Science and Machine Learning',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'APIs',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Algorithmic Trading',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ];
+    queryInterface.bulkInsert('skills', startingSkills);
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.bulkDelete('skills', null);
+  },
+};
