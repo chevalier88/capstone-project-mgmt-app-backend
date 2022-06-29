c957dff07a67a5cca1584b80bf8a3370ee39fbe2
Graham <graham.lim88@gmail.com>
2022-06-29 16:50:07 +0800

    comments seeding done, all seeding is now completed

---
+++ seeders/20220629084517-comments-data.js
@@ -0,0 +1,25 @@
+module.exports = {
+  up: async (queryInterface) => {
+    const startingComments = [
+      {
+        user_id: 1,
+        project_id: 1,
+        comment: 'wow what a doge',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        user_id: 2,
+        project_id: 1,
+        comment: 'yeah dude dogeness is a fact.',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ];
+    await queryInterface.bulkInsert('comments', startingComments);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('comments', null);
+  },
+};
