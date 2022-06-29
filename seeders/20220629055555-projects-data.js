9e7779f28de5a52dda42339693687eed2dc24a97
Graham <graham.lim88@gmail.com>
2022-06-29 15:55:04 +0800

    project seeders data completed, now writing through tables

--- seeders/20220629055555-projects-data.js
+++ seeders/20220629055555-projects-data.js
@@ -1,24 +1,87 @@
-'use strict';
-
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface) => {
+    const data1 = {
+      lanes: [
+        {
+          id: 'lane1',
+          title: 'Planned Tasks',
+          label: '2/2',
+          cards: [
+            {
+              id: 'Card1', title: 'placeholder_1', description: 'bla bla', label: '30 mins', draggable: false,
+            },
+            {
+              id: 'Card2', title: 'placeholder_2', description: 'lorem ipsum', label: '5 mins', metadata: { sha: 'be312a1' },
+            },
+          ],
+        },
+        {
+          id: 'lane2',
+          title: 'Completed',
+          label: '0/0',
+          cards: [],
+        },
+      ],
+    };
+    const data2 = {
+      lanes: [
+        {
+          id: 'lane1',
+          title: 'Planned Tasks',
+          label: '2/2',
+          cards: [
+            {
+              id: 'Card1', title: 'placeholder_1', description: 'bla bla', label: '30 mins', draggable: false,
+            },
+            {
+              id: 'Card2', title: 'placeholder_2', description: 'lorem ipsum', label: '5 mins', metadata: { sha: 'be312a1' },
+            },
+          ],
+        },
+        {
+          id: 'lane2',
+          title: 'Completed',
+          label: '0/0',
+          cards: [],
+        },
+      ],
+    };
+    const startingProjects = [
+      {
+        name: 'Leave Application App',
+        summary: 'A HR Client is looking to build a leave application app where users will be able to submit, approve and plan their leave in an enterprise.',
+        no_engineers_required: 3,
+        minimum_salary: 45.00,
+        enrolment_deadline: new Date('September 1, 2022 00:00:00'),
+        delivery_deadline: new Date('December 1, 2022 00:00:00'),
+        kanban_data: JSON.stringify(data1),
+        stage: 'sourcing',
+        projected_hours: 120,
+        actual_hours: 0,
+        industry_id: 4,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Job Application App',
+        summary: 'A Market Research Client is looking to build a job application app where users will be able to apply for jobs, and this data can be tracked.',
+        no_engineers_required: 2,
+        minimum_salary: 25.00,
+        enrolment_deadline: new Date('September 2, 2022 00:00:00'),
+        delivery_deadline: new Date('December 15, 2022 00:00:00'),
+        kanban_data: JSON.stringify(data2),
+        stage: 'sourcing',
+        projected_hours: 80,
+        actual_hours: 0,
+        industry_id: 3,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ];
+    queryInterface.bulkInsert('projects', startingProjects);
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  down: async (queryInterface) => {
+    await queryInterface.bulkDelete('projects', null);
+  },
 };
